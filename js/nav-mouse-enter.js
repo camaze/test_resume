95a430111500bf340c508ec82ae72a402f43105e
Nian <1046783533@qq.com>
2020-10-06 20:40:08 +0800

    MVC structure supported

---
+++ js/nav-mouse-enter.js
@@ -0,0 +1,44 @@
+// 鼠标进入导航的li（关于等时），会有小红条，而且在作品那里有显示作品1-3
+! function () {
+    let view = document.querySelectorAll('nav.menu > ul >li')
+    var controller = {
+        view: null,
+        init: function () {
+            this.view = view
+            this.bindEvents()
+        },
+        bindEvents: function () {
+            for (let i = 0; i < this.view.length; i++) {
+                // 鼠标进入就反应
+                this.view[i].onmouseenter = function (x) {
+                    let li = x.currentTarget
+                    li.classList.add('active')
+
+                    // let brother = li.getElementsByTagName('ul')[0]
+                    // brother.classList.add('active')
+
+                    // let brother = a.nextSibling
+                    // 害怕取到文本节点
+                    // while (brother.tagName !== 'UL') {
+                    //     brother = brother.nextSibling
+                    // }
+
+                }
+                this.view[i].onmouseleave = function (x) {
+                    let li = x.currentTarget
+                    li.classList.remove('active')
+
+
+                    // let brother = li.getElementsByTagName('ul')[0]
+
+                    // brother.classList.remove('active')
+                    // 害怕取到文本节点
+                    // while (brother.tagName !== 'UL') {
+                    //     brother = brother.nextSibling
+                    // }
+                }
+            }
+        }
+    }
+    controller.init(view)
+}.call()
--- js/topbar-add-sticky.js
+++ js/topbar-add-sticky.js
@@ -1,13 +1,38 @@
-if (window.scrollY > 0) { // 初始状态就不在页面最上面
-    setTimeout(function () {
-        topNavBar.classList.add('sticky')
-    }, 1500)
-}
+! function () {
+    var view = document.querySelector('#topNavBar')
 
-window.addEventListener('scroll', function (xxx) {
-    if (window.scrollY > 0) {
-        topNavBar.classList.add('sticky')
-    } else {
-        topNavBar.classList.remove('sticky')
+    var controller = {
+        view: null,
+        init: function (view) { //init 只赋值 *******
+            this.view = view
+            this.bindEvents() //绑定事件
+            // this.bindEvents.call(this)
+        },
+        bindEvents: function () {
+            var view = this.view
+            if (window.scrollY > 0) { // 初始状态就不在页面最上面
+                setTimeout(function () {
+                    this.active()
+                }.bind(this), 1500) // 注意bind(xxx)，使得新函数的this指定为xxx
+            }
+
+            window.addEventListener('scroll', (xxx) => {
+                if (window.scrollY > 0) {
+                    this.active()
+                } else {
+                    this.deactive()
+                }
+            })
+            // 箭头函数没有this
+        },
+        active: function () {
+            this.view.classList.add('sticky')
+        },
+        deactive: function () {
+            this.view.classList.remove('sticky')
+        }
+
     }
-})
+    controller.init(view)
+    // controller.init.call(controller, view)
+}.call()
